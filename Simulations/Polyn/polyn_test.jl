a20d40d337d03ac546ec2741c64a5191cfdbd3c0
nkashaev <36710350+nkashaev@users.noreply.github.com>
2021-11-28 10:56:14 -0500

    typos

--- Simulations/Polyn/polyn_test.jl
+++ Simulations/Polyn/polyn_test.jl
@@ -5,6 +5,7 @@ using Random, Distributions, LinearAlgebra
 using JuMP, SCS
 using Plots, Clustering
 using LowRankApprox, Printf
+using Optim
 #Dir
 tempdir1=@__DIR__
 rootdir=tempdir1[1:findfirst("PPPP",tempdir1)[end]]
@@ -17,14 +18,14 @@ include(dirmain*"mixSQP.jl")
 pis=[2.0, 2.5, 3.0, 5.5];
 p=[0.4,0.4,0.1,0.1];
 seed=10;
-n=100000;
+n=1000;
 data=dgp(pis,p,n,seed);
 #histogram(data)
 
 
 ## SQP method
 npoints=100
-θ=[minimum(data)+k*(maximum(data)-minimum(data))/dtheta for k in 2:dtheta-2]
+θ=[minimum(data)+k*(maximum(data)-minimum(data))/npoints for k in 2:npoints-2]
 out = mixSQP(MatA(θ,data),maxiter = 10,verbose = false);
 out1 = mixSQP(MatA(θ,data),x = ones(length(θ))/length(θ));
 
@@ -36,12 +37,12 @@ p_raw=p_raw[p_raw .>τ]
 
 #Clustering θ to T types
 T=4
-cl_θ=kmeans(θ_raw', T; weights=p_hat_raw)
+cl_θ=kmeans(θ_raw', T; weights=p_raw)
 θ_hat=sort(cl_θ.centers[:]) #Intial guess for θ
 out2 = mixSQP(MatA(θ_hat,data),x = ones(length(θ_hat))/length(θ_hat));
-p_hat=out["x"] #Intial guess for p
+p_hat=out2["x"] #Intial guess for p
 
-using Optim
+#Final optimization
 func2(vars)=-loglike(vars,data)
 #∇func2!(G,vars)=-∇loglike!(G,vars,data)
 param_ini=vcat(θ_hat,log.([0.2,0.2,0.2]))
